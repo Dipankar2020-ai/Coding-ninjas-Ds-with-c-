5b7c70d16048caf42324078bae2566220381f5b0
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-06 12:47:11 +0530

    Add files via upload

---
+++ Tree/delete treenode.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include<queue>
+using namespace std;
+template<typename T>
+class Treenode
+{
+    public:
+    T data;
+    vector<Treenode*>children;
+    Treenode(T data)
+    {
+        this->data=data;
+    }
+    ~Treenode()
+    {
+        for(int i=0;i<children.size();i++)
+        {
+            delete children[i];
+        }
+    }
+};
+Treenode<int>* createlevel()
+{
+    int rootdata;
+    cout<<"Enter root data : ";
+    cin>>rootdata;
+     Treenode<int>*  root=new Treenode<int>(rootdata);
+    queue<Treenode<int>*>pendingnodes;
+     pendingnodes.push(root);
+    while(pendingnodes.size()!=0)
+    {
+        Treenode<int>* front=pendingnodes.front();
+        pendingnodes.pop();
+        int numdata;
+        
+        cout<<"enter the number of children of : "<<front->data<<"=>";
+        cin>>numdata;
+        for(int i=0;i<numdata;i++)
+        {
+            int childata;
+            cout<<"Enter "<<i<<" th children of :"<<front->data<<"=>";
+            cin>>childata;
+            cout<<endl;
+             Treenode<int>* child= new Treenode<int>(childata);
+             front->children.push_back(child);
+             pendingnodes.push(child);
+        }
+    }
+    return root;
+    
+    
+}
+int numnodes(Treenode<int>* root)
+{
+    int ans=1;
+    for(int i=0;i<root->children.size();i++)
+    {
+        ans=ans+numnodes(root->children[i]);
+    }
+    return ans;
+}
+void printlevelwise(Treenode<int>* root)
+{
+    
+    queue<Treenode<int>*>pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0)
+    {
+        cout<<pendingnodes.front()->data<<":";
+        for(int i=0;i<pendingnodes.front()->children.size();i++)
+        {
+            cout<<pendingnodes.front()->children[i]->data;
+            pendingnodes.push(pendingnodes.front()->children[i]);
+        
+        if(i<pendingnodes.front()->children.size()-1)
+        {
+            cout<<",";
+        }
+        }
+        cout<<endl;
+        pendingnodes.pop();
+        
+    }
+}
+void preorder(Treenode<int>*root)
+{
+    if(root==NULL)
+     {
+        return;
+    }
+    cout<<root->data<<" ";
+    for(int i=0;i<root->children.size();i++)
+    {
+        preorder(root->children[i]);
+    }
+}
+void postorder(Treenode<int>*root)
+{
+    if(root==NULL)
+     {
+        return;
+    }
+   
+    for(int i=0;i<root->children.size();i++)
+    {
+         
+        postorder(root->children[i]);
+     
+    }
+       cout<<root->data<<" ";
+}
+void deletenode(Treenode<int>* root)
+{
+    for(int i=0;i<root->children.size();i++)
+    {
+        deletenode(root->children[i]);
+    }
+    delete root;
+}
+
+int main()
+{
+     Treenode<int>* root=createlevel();
+     //printlevelwise(root);
+     postorder(root);
+     //deletenode(root); we can delete node by two ways one is destructor and another is deletenode method 
+     delete root;
+     postorder(root);
+     
+
+    return 0;
+}
