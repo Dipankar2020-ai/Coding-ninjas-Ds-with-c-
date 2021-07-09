3c45f885b75a2a022c0d21c8e3f4e33d4f4d656d
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-09 11:35:04 +0530

    Add files via upload

---
+++ Binary Tree/inorder traversal in binary tree.cpp
@@ -0,0 +1,117 @@
+
+#include <iostream>
+#include<queue>
+using namespace std;
+template<typename T>
+
+class Binarynode
+{
+    public:
+    T data;
+    Binarynode* left;
+    Binarynode* right;
+    Binarynode(T data)
+    {
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+    ~Binarynode()
+    {
+        delete left;
+        delete right;
+    }
+    
+};
+Binarynode<int>* takeinputlevelwise()
+{
+    int rootdata;
+    cout<<"Enter root data: "<<endl;
+    cin>>rootdata;
+   
+    if(rootdata==-1)
+    {
+        return NULL;
+    }
+    Binarynode<int>* root=new Binarynode<int>(rootdata);
+    queue<Binarynode<int>*>pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0)
+    {
+        Binarynode<int>* front=pendingnodes.front();
+        pendingnodes.pop();
+        int leftchild;
+        cout<<"Enter left child of :"<<front->data<<"=>";
+        cin>>leftchild;
+        if(leftchild!=-1)
+        {
+            Binarynode<int>* child=new Binarynode<int>(leftchild);
+            front->left=child;
+            pendingnodes.push(child);
+            
+            
+        }
+        int rightchild;
+        cout<<"Enter right child of :"<<front->data<<"=>";
+        cin>>rightchild;
+        if(rightchild!=-1)
+        {
+            Binarynode<int>* child=new Binarynode<int>(rightchild);
+            front->right=child;
+            pendingnodes.push(child);
+            
+            
+        }
+    }
+    return root;
+    
+}
+void printlevelwise(Binarynode<int>* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    queue<Binarynode<int>*>pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0)
+    {
+        cout<<pendingnodes.front()->data<<":";
+        if(pendingnodes.front()->left==NULL)
+        {
+            cout<<"L:-1";
+        }
+        if(pendingnodes.front()->left!=NULL)
+        {
+            cout<<"L:"<<pendingnodes.front()->left->data<<",";
+            pendingnodes.push(pendingnodes.front()->left);
+        }
+         if(pendingnodes.front()->right==NULL)
+        {
+            cout<<"R:-1";
+        }
+        if(pendingnodes.front()->right!=NULL)
+        {
+            cout<<"R:"<<pendingnodes.front()->right->data<<",";
+            pendingnodes.push(pendingnodes.front()->right);
+        }
+        cout<<endl;
+        pendingnodes.pop();
+    }
+}
+    void inorder(Binarynode<int>* root)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+    
+ int main()
+ {
+    Binarynode<int>* root=takeinputlevelwise();
+   inorder(root);
+ }
