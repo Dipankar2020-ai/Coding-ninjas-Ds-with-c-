663e01500cd468bc437b2a4299b928c1ecdefce1
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-10 21:33:10 +0530

    Add files via upload

---
+++ BST/elements in range in bst.cpp
@@ -0,0 +1,139 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include<queue>
+#include<vector>
+#include<stack>
+using namespace std;
+template<typename T>
+
+class Binarynode
+{
+    public:
+    T data;
+    Binarynode* left;
+    Binarynode* right;
+    Binarynode(T data)
+    {
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+    ~Binarynode()
+    {
+        delete left;
+        delete right;
+    }
+    
+};
+
+
+
+Binarynode<int>* takeinputlevelwise()
+{
+    int rootdata;
+    cout<<"Enter root data: "<<endl;
+    cin>>rootdata;
+   
+    if(rootdata==-1)
+    {
+        return NULL;
+    }
+    Binarynode<int>* root=new Binarynode<int>(rootdata);
+    queue<Binarynode<int>*>pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0)
+    {
+        Binarynode<int>* front=pendingnodes.front();
+        pendingnodes.pop();
+        int leftchild;
+        cout<<"Enter left child of :"<<front->data<<"=>";
+        cin>>leftchild;
+        if(leftchild!=-1)
+        {
+            Binarynode<int>* child=new Binarynode<int>(leftchild);
+            front->left=child;
+            pendingnodes.push(child);
+            
+            
+        }
+        int rightchild;
+        cout<<"Enter right child of :"<<front->data<<"=>";
+        cin>>rightchild;
+        if(rightchild!=-1)
+        {
+            Binarynode<int>* child=new Binarynode<int>(rightchild);
+            front->right=child;
+            pendingnodes.push(child);
+            
+            
+        }
+    }
+    return root;
+    
+}
+void printlevelorder(Binarynode<int>* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    queue<Binarynode<int>*>pendingnodes;
+    pendingnodes.push(root);
+    pendingnodes.push(NULL);
+    while(pendingnodes.size()!=0)
+    {
+        if(pendingnodes.front()==NULL)
+        {
+            pendingnodes.pop();
+            if(pendingnodes.size()==0)
+            {
+                break;
+            }
+            pendingnodes.push(NULL);
+            cout<<endl;
+        }
+        if(pendingnodes.front()->left!=NULL)
+        {
+            pendingnodes.push(pendingnodes.front()->left);
+        }
+         if(pendingnodes.front()->right!=NULL)
+        {
+            pendingnodes.push(pendingnodes.front()->right);
+        }
+        cout<<pendingnodes.front()->data<<" ";
+        pendingnodes.pop();
+    }
+}
+void printelements(Binarynode<int>* root,int k1,int k2)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    if(root->data>k1)
+    {
+        printelements(root->left,k1,k2);
+    }
+   if(root->data>=k1 && root->data<=k2)
+    {
+        cout<<root->data<<" ";
+    }
+    if(root->data<k2)
+    {
+         printelements(root->right,k1,k2);
+    }
+    
+   
+   
+}
+
+ int main()
+ {
+     
+    
+     Binarynode<int>* root=takeinputlevelwise();
+     int k1,k2;
+     cin>>k1>>k2;
+     printelements(root,k1,k2);
+     
+ } 
