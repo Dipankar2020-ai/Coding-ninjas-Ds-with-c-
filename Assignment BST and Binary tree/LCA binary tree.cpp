f9378b3c882f8d49437fdfba50d46ec167cbe0d9
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-12 09:46:03 +0530

    Add files via upload

---
+++ Assignment BST and Binary tree/LCA binary tree.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#include<queue>
+#include<vector>
+#include<stack>
+using namespace std;
+int k=0;
+template<typename T>
+
+class Binarynode
+{
+    public:
+    T data;
+    Binarynode* left;
+    Binarynode* right;
+    Binarynode(T data)
+    {
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+    ~Binarynode()
+    {
+        delete left;
+        delete right;
+    }
+    
+};
+
+
+
+
+Binarynode<int>* takeinputlevelwise()
+{
+    int rootdata;
+    cout<<"Enter root data: "<<endl;
+    cin>>rootdata;
+   
+    if(rootdata==-1)
+    {
+        return NULL;
+    }
+    Binarynode<int>* root=new Binarynode<int>(rootdata);
+    queue<Binarynode<int>*>pendingnodes;
+    pendingnodes.push(root);
+    while(pendingnodes.size()!=0)
+    {
+        Binarynode<int>* front=pendingnodes.front();
+        pendingnodes.pop();
+        int leftchild;
+        cout<<"Enter left child of :"<<front->data<<"=>";
+        cin>>leftchild;
+        if(leftchild!=-1)
+        {
+            Binarynode<int>* child=new Binarynode<int>(leftchild);
+            front->left=child;
+            pendingnodes.push(child);
+            
+            
+        }
+        int rightchild;
+        cout<<"Enter right child of :"<<front->data<<"=>";
+        cin>>rightchild;
+        if(rightchild!=-1)
+        {
+            Binarynode<int>* child=new Binarynode<int>(rightchild);
+            front->right=child;
+            pendingnodes.push(child);
+            
+            
+        }
+    }
+    return root;
+    
+}
+void printlevelorder(Binarynode<int>* root)
+{
+    if(root==NULL)
+    {
+        return;
+    }
+    queue<Binarynode<int>*>pendingnodes;
+    pendingnodes.push(root);
+    pendingnodes.push(NULL);
+    while(pendingnodes.size()!=0)
+    {
+        if(pendingnodes.front()==NULL)
+        {
+            pendingnodes.pop();
+            if(pendingnodes.size()==0)
+            {
+                break;
+            }
+            pendingnodes.push(NULL);
+            cout<<endl;
+        }
+        if(pendingnodes.front()->left!=NULL)
+        {
+            pendingnodes.push(pendingnodes.front()->left);
+        }
+         if(pendingnodes.front()->right!=NULL)
+        {
+            pendingnodes.push(pendingnodes.front()->right);
+        }
+        cout<<pendingnodes.front()->data<<" ";
+        pendingnodes.pop();
+    }
+}
+int lcabinarytree(Binarynode<int>* root,int val1,int val2)
+{
+    if(root==NULL)
+    {
+        return NULL;
+    }
+    if(root->data==val1 || root->data==val2)
+    {
+        return root->data;
+    }
+    int a=lcabinarytree(root->left,val1,val2);
+    int b=lcabinarytree(root->right,val1,val2);
+    if(a==NULL && b==NULL)
+    {
+        return NULL;
+    }
+    else if(a==NULL && b!=NULL)
+    {
+        return b;
+    }
+    else if(a!=NULL && b==NULL)
+    {
+        return a;
+    }
+    else{
+        return root->data;
+    }
+    
+}
+int main()
+{
+    Binarynode<int>* root=takeinputlevelwise();
+    int a,b;
+    cin>>a>>b;
+    cout<<lcabinarytree(root,a,b);
+    
+}
