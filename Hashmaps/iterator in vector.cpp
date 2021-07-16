14a7b3adac14edf41a2e465c852e69ce0b1a6ea2
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2021-07-16 02:55:38 -0700

    Add files via upload

---
+++ Hashmaps/iterator in vector.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<unordered_map>
+#include<vector> 
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    
+    vector<int>::iterator it=v.begin();
+    while(it!=v.end())
+    {
+        cout<<*it<<endl;
+        it++;
+    }
+        
+    
+
+    return 0;
+}
